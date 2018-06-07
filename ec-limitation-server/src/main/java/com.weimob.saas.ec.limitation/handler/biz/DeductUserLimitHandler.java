b10f77daf8d8cc6670655ee4f09966f066a6caf0
pengqin.zhou <pengqin.zhou@weimob.com>
2018-06-07 16:56:02 +0800

    减少限购handler

--- ec-limitation-server/src/main/java/com.weimob.saas.ec.limitation/handler/biz/DeductUserLimitHandler.java
+++ ec-limitation-server/src/main/java/com.weimob.saas.ec.limitation/handler/biz/DeductUserLimitHandler.java
@@ -1,13 +1,58 @@
 package com.weimob.saas.ec.limitation.handler.biz;
 
+import com.weimob.saas.ec.limitation.exception.LimitationBizException;
+import com.weimob.saas.ec.limitation.exception.LimitationErrorCode;
 import com.weimob.saas.ec.limitation.handler.BaseHandler;
 import com.weimob.saas.ec.limitation.model.request.UpdateUserLimitVo;
+import com.weimob.saas.ec.limitation.service.LimitationServiceImpl;
+import com.weimob.saas.ec.limitation.utils.LimitContext;
+import com.weimob.saas.ec.limitation.utils.VerifyParamUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author lujialin
  * @description 减少限购handler
+ * @scenes 下单成功
  * @date 2018/6/6 14:22
  */
 public class DeductUserLimitHandler extends BaseHandler<UpdateUserLimitVo> {
+
+    @Autowired
+    private LimitationServiceImpl limitationService;
+
+    @Override
+    protected void doBatchBizLogic(List<UpdateUserLimitVo> vos) {
+
+        Map<String, Integer> orderGoodsLimitMap = new HashMap<>();
+
+        if (Objects.equals(vos.get(0).getBizType(), 30)) {
+            super.updateUserLimitRecord(orderGoodsLimitMap, null, null, false);
+        }
+    }
+
+    @Override
+    protected void checkParams(List<UpdateUserLimitVo> vos) {
+        super.checkParams(vos);
+        // TODO 重复代码，抽象出来
+        for (UpdateUserLimitVo limitVo : vos) {
+            VerifyParamUtils.checkParam(LimitationErrorCode.PID_IS_NULL, limitVo.getPid());
+            VerifyParamUtils.checkParam(LimitationErrorCode.STORE_IS_NULL, limitVo.getStoreId());
+            VerifyParamUtils.checkParam(LimitationErrorCode.GOODSID_IS_NULL, limitVo.getGoodsId());
+            VerifyParamUtils.checkParam(LimitationErrorCode.BIZTYPE_IS_NULL, limitVo.getBizType());
+            VerifyParamUtils.checkParam(LimitationErrorCode.BIZID_IS_NULL, limitVo.getBizId());
+            VerifyParamUtils.checkParam(LimitationErrorCode.GOODSNUM_IS_NULL, limitVo.getGoodsNum());
+            if (limitVo.getGoodsNum() < 1) {
+                throw new LimitationBizException(LimitationErrorCode.GOODSNUM_IS_ILLEGAL);
+            }
+            VerifyParamUtils.checkParam(LimitationErrorCode.ORDERNO_IS_NULL, limitVo.getOrderNo());
+            VerifyParamUtils.checkParam(LimitationErrorCode.WID_IS_NULL, limitVo.getWid());
+        }
+    }
+
 
 }
