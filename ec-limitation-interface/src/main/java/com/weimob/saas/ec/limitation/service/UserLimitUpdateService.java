462b5115fc2b0050470e8c2bd9f8501f52cd7062
jialin.lu <jialin.lu@weimob.com>
2018-06-08 10:56:56 +0800

    类型为特权价的下单限购校验代码修改

--- ec-limitation-interface/src/main/java/com/weimob/saas/ec/limitation/service/UserLimitUpdateService.java
+++ ec-limitation-interface/src/main/java/com/weimob/saas/ec/limitation/service/UserLimitUpdateService.java
@@ -15,6 +15,17 @@ import com.weimob.soa.common.response.SoaResponse;
  */
 public interface UserLimitUpdateService {
 
+
+    /**
+     * @param
+     * @return
+     * @title 下单限购校验
+     * @author lujialin
+     * @date 2018/6/8 9:46
+     * @useScene 下单限购校验
+     * @parameterExample {	"updateUserLimitVoList": [{			"bizId": 212122,			"bizType": 30,			"pid": 1000,			"storeId": 200,			"goodsId": 32783928,			"orderNo": 123233132323,			"goodsNum": 1,			"wid": 212123		},		{			"bizId": 212122,			"bizType": 30,			"pid": 1000,			"storeId": 200,			"goodsId": 32783920,			"orderNo": 123233132323,			"goodsNum": 1,			"wid": 212123		}	]}
+     * @returnExample {	"errT":null,	"globalTicket":null,	"logBizData":null,	"monitorTrackId":"811aa9c3-d8dc-4b6f-b435-5c73fd2d1df8",	"processResult":true,	"responseVo":{		"ticket":"811aa9c3-d8dc-4b6f-b435-5c73fd2d1df8"	},	"returnCode":"000000",	"returnMsg":null,	"successForMornitor":true,	"timestamp":"1528422331387"}
+     */
     SoaResponse<UpdateUserLimitResponseVo, LimitationErrorCode> saveUserLimit(SaveUserLimitRequestVo requestVo);
 
 
