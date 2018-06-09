effcc88c6d850fdd02239638cb329c1fbb74f8e9
jialin.lu <jialin.lu@weimob.com>
2018-06-09 10:53:20 +0800

    增加接口注释

--- ec-limitation-interface/src/main/java/com/weimob/saas/ec/limitation/service/UserLimitUpdateService.java
+++ ec-limitation-interface/src/main/java/com/weimob/saas/ec/limitation/service/UserLimitUpdateService.java
@@ -28,9 +28,29 @@ public interface UserLimitUpdateService {
      */
     SoaResponse<UpdateUserLimitResponseVo, LimitationErrorCode> saveUserLimit(SaveUserLimitRequestVo requestVo);
 
-
+    /**
+     *
+     * @title 取消订单，减少限购记录
+     * @author lujialin
+     * @date 2018/6/9 10:48
+     * @useScene
+     * @parameterExample {	"updateUserLimitVoList": [{			"bizId": 2212122,			"bizType": 10,			"pid": 1000,			"storeId": 200,			"goodsId": 32700928,                        "skuId":21038219,			"orderNo": 123233132323,			"goodsNum": 1,			"wid": 212123		}	]}
+     * @returnExample {	"errT":null,	"globalTicket":null,	"logBizData":null,	"monitorTrackId":"811aa9c3-d8dc-4b6f-b435-5c73fd2d1df8",	"processResult":true,	"responseVo":{		"ticket":"811aa9c3-d8dc-4b6f-b435-5c73fd2d1df8"	},	"returnCode":"000000",	"returnMsg":null,	"successForMornitor":true,	"timestamp":"1528422331387"}
+     * @param
+     * @return
+     */
     SoaResponse<UpdateUserLimitResponseVo, LimitationErrorCode> deductUserLimit(DeductUserLimitRequestVo requestVo);
 
-
+    /**
+     *
+     * @title 回滚限购记录
+     * @author lujialin
+     * @date 2018/6/9 10:50
+     * @useScene
+     * @parameterExample {	"ticket": "bdd2572e-8cfe-4a73-8179-b6adec666ebe"}
+     * @returnExample {	"errT": null,	"globalTicket": null,	"logBizData": null,	"monitorTrackId": "532df4a6-29c9-439d-a92c-1baa684c1850",	"processResult": true,	"responseVo": {		"status": true	},	"returnCode": "000000",	"returnMsg": "处理成功",	"successForMornitor": true,	"timestamp": "1528426520636"}
+     * @param
+     * @return
+     */
     SoaResponse<ReverseUserLimitResponseVo, LimitationErrorCode> reverseUserLimit(ReverseUserLimitRequestVo requestVo);
 }
